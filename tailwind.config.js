2791d5f89611f476c2c90d1df6aafba805c4a952
Jerome Paulos <jeromepaulos@icloud.com>
2025-02-22 15:38:58 -0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './resources/**/*.php',
+        './app/Livewire/**/*.php'
+    ],
+    theme: {},
+    plugins: []
+};
