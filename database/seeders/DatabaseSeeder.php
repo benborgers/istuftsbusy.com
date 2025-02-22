f5e32d488d12c12d738784f9295b2e055ff612dd
Jerome Paulos <jeromepaulos@icloud.com>
2025-02-22 17:01:56 -0500

    Create `Location` model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Location;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -13,5 +14,20 @@ class DatabaseSeeder extends Seeder
             'name' => 'Jerome Paulos',
             'email' => 'jero@hey.com'
         ]);
+
+        Location::create(
+            [
+                'name' => 'Joyce Cummings Center',
+                'informal_name' => 'JCC'
+            ],
+            [
+                'name' => 'Tisch Sports and Fitness Center',
+                'informal_name' => 'Gym'
+            ],
+            [
+                'name' => 'Tisch Library',
+                'informal_name' => 'Tisch Library'
+            ]
+        );
     }
 }
