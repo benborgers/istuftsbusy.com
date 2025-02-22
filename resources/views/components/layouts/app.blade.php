c457e91b91560c5a0b849e79e9276262159113b1
Jerome Paulos <jeromepaulos@icloud.com>
2025-02-22 16:01:46 -0500

    Add 🐘 favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>{{ $title }}</title>
+    <link rel="icon" href="https://emojicdn.elk.sh/🐘">
 
     @vite(['resources/css/app.css'])
 </head>
