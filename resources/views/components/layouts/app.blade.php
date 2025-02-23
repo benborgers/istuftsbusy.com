38ead77b4f4fe8065205dedc43d02f0c1b18e7c7
Jerome Paulos <jeromepaulos@icloud.com>
2025-02-23 15:42:05 -0500

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -13,6 +13,11 @@
     <link href="https://api.fontshare.com/v2/css?f[]=clash-grotesk@400,500,600,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap" rel="stylesheet">
 
+    @production
+        <script async src="https://beamanalytics.b-cdn.net/beam.min.js"
+            data-token="dc450d08-0ea1-4a44-8ba3-ef4139b189a1"></script>
+    @endproduction
+
     @fluxAppearance
 </head>
 
