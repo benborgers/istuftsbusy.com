c5eb7eb3fa209517b75aa84f13ea3ed92d1de4eb
Jerome Paulos <jeromepaulos@icloud.com>
2025-02-23 15:07:53 -0500

    Update home.blade.php

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -1,6 +1,6 @@
 @use(App\Support\Busyness)
 
-<div class="min-h-dvh flex flex-col gap-6 p-6" wire:poll.5s>
+<div class="min-h-dvh flex flex-col gap-6 p-6">
     <div>
         <h1 class="text-4xl font-serif text-center">
             <span>Is</span>
