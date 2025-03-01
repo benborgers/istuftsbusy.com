0774a26535fae8b68435dfca2a89fba60facf6d4
Jerome Paulos <jeromepaulos@icloud.com>
2025-03-01 17:12:57 -0500

    Gray out offline locations

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -11,37 +11,46 @@
 
     <flux:accordion transition exclusive>
         @foreach($this->locations as $location)
-            <flux:accordion.item :expanded="$loop->first">
-                <flux:accordion.heading class="hover:bg-zinc-600/5 rounded-lg p-2 transition-colors">
-                    <div class="flex gap-4 justify-between items-center">
-                        <div class="line-clamp-1 text-lg leading-none">
-                            <p>{{ $location->informal_name }}</p>
+            @php($lastScanDate = $location->lastScanDate())
 
-                            @if($location->lastScanDate())
+            @if($lastScanDate !== null)
+                @php($isOffline = $lastScanDate->diffInMinutes() > 30)
+
+                <flux:accordion.item :expanded="$loop->first && !$isOffline" :disabled="$isOffline">
+                    <flux:accordion.heading @class(['rounded-lg p-2', 'hover:bg-zinc-600/5 transition-colors' => !$isOffline])>
+                        <div class="flex gap-4 justify-between items-center">
+                            <div class="line-clamp-1 text-lg leading-none">
+                                <p>{{ $location->informal_name }}</p>
+
                                 <div class="flex gap-2 items-center ml-1">
                                     <div class="relative *:size-2 *:rounded-full">
-                                        <div class="bg-accent"></div>
-                                        <div class="absolute animate-ping inset-0 bg-accent/50"></div>
+                                        @if($isOffline)
+                                            <div class="bg-rose-500"></div>
+                                            <div class="absolute animate-ping inset-0 bg-rose-500/50"></div>
+                                        @else
+                                            <div class="bg-accent"></div>
+                                            <div class="absolute animate-ping inset-0 bg-accent/50"></div>
+                                        @endif
                                     </div>
 
                                     <p class="text-zinc-400 text-sm font-medium">
-                                        {{ $location->lastScanDate()->diffForHumans() }}
+                                        {{ $lastScanDate->diffForHumans() }}
                                     </p>
                                 </div>
-                            @endif
+                            </div>
+
+                            @php($busyness = $location->currentBusyness())
+                            <flux:badge color="{{ $busyness->color() }}">
+                                {{ $busyness->label() }}
+                            </flux:badge>
                         </div>
+                    </flux:accordion.heading>
 
-                        @php($busyness = $location->currentBusyness())
-                        <flux:badge color="{{ $busyness->color() }}">
-                            {{ $busyness->label() }}
-                        </flux:badge>
-                    </div>
-                </flux:accordion.heading>
-
-                <flux:accordion.content>
-                    <livewire:location-chart :$location />
-                </flux:accordion.content>
-            </flux:accordion.item>
+                    <flux:accordion.content>
+                        <livewire:location-chart :$location />
+                    </flux:accordion.content>
+                </flux:accordion.item>
+            @endif
         @endforeach
     </flux:accordion>
 
